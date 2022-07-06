3a203bd012a835adee49cf034a2b24dd70241401
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-07-06 20:13:51 +0500

    Update models

--- models/comment.js
+++ models/comment.js
@@ -5,10 +5,7 @@ const CommentSchema = new Schema({
   author_name: { type: String, required: true },
   text: { type: String, required: true },
   post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
-});
-
-CommentSchema.virtual('comment-date').get(function () {
-  return new Date();
+  date: { type: Date, require: true, default: new Date() },
 });
 
 module.exports = mongoose.model('Comment', CommentSchema);
--- models/post.js
+++ models/post.js
@@ -5,10 +5,7 @@ const PostSchema = new Schema({
   title: { type: String, requied: true },
   content: { type: String, requied: true },
   published: { type: Boolean, required: true, default: false },
-});
-
-PostSchema.virtual('publish-date').get(function () {
-  return new Date();
+  publish_date: { type: Date },
 });
 
 module.exports = mongoose.model('Post', PostSchema);
