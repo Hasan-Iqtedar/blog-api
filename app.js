ed7f13fa8e6c28442b15a98119223d1f95fde8b7
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-07-14 12:26:10 +0500

    Ready for production

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var cors = require('cors');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
 
@@ -23,10 +25,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(cors());
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/posts', indexRouter.post);
